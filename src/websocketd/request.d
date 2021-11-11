cfb7ed5df3d319198e8dfc4ce956417dcbcedd94
0-v-0 <kduice@qq.com>
2021-11-11 21:04:52 +0800

    feat: combine `send` method & cleanup

--- src/websocketd/request.d
+++ src/websocketd/request.d
@@ -1,101 +1,98 @@
 module websocketd.request;
 
-import std.conv : to;
 import std.ascii : isWhite;
 import std.algorithm : endsWith;
 
 struct Request {
-   string method;
-   string path;
-   string httpVersion;
-   string[string] headers;
-   string message;
-   bool done = false;
+	string method, path, httpVersion;
+	string[string] headers;
+	string message;
+	bool done;
 
-   static Request parse(ubyte[] bytes) {
-      static ubyte[] data = [];
-      static Request req = Request();
+	static Request parse(ubyte[] bytes) {
+		static ubyte[] data;
+		static Request req = Request();
 
-      data ~= bytes;
-      string message = (cast(char[])data).to!string;
-      if (!message.endsWith("\r\n\r\n"))
-         return req;
+		data ~= bytes;
+		auto msg = cast(string)data;
+		if (!msg.endsWith("\r\n\r\n"))
+			return req;
 
-      size_t i = 0;
-      string token = "";
+		size_t i = 0;
+		string token;
 
-      // get method
-      for (; i < message.length; i++) {
-         if (message[i] == ' ')
-            break;
-         token ~= message[i];
-      }
-      i++; // skip whitespace
-      req.method = token;
-      token = "";
+		// get method
+		for (; i < msg.length; i++) {
+			if (msg[i] == ' ')
+				break;
+			token ~= msg[i];
+		}
+		i++; // skip whitespace
+		req.method = token;
+		token = "";
 
-      // get path
-      for (; i < message.length; i++) {
-         if (message[i] == ' ')
-            break;
-         token ~= message[i];
-      }
-      i++;
-      req.path = token;
-      token = "";
+		// get path
+		for (; i < msg.length; i++) {
+			if (msg[i] == ' ')
+				break;
+			token ~= msg[i];
+		}
+		i++;
+		req.path = token;
+		token = "";
 
-      // get version
-      for (; i < message.length; i++) {
-         if (message[i] == '\r')
-            break;
-         token ~= message[i];
-      }
-      i++; // skip \r
-      if (message[i] != '\n')
-         return req;
-      i++;
-      req.httpVersion = token;
-      token = "";
+		// get version
+		for (; i < msg.length; i++) {
+			if (msg[i] == '\r')
+				break;
+			token ~= msg[i];
+		}
+		i++; // skip \r
+		if (msg[i] != '\n')
+			return req;
+		i++;
+		req.httpVersion = token;
+		token = "";
 
-      // get headers
-      string key = "";
-      for (; i < message.length; i++) {
-         token = "";
-         key = "";
-         if (message[i] == '\r')
-            break;
-         for (; i < message.length; i++) {
-            if (message[i] == ':' || message[i].isWhite)
-               break;
-            token ~= message[i];
-         }
-         i++;
-         key = token;
-         token = "";
-         for (; i < message.length; i++)
-            if (!message[i].isWhite)
-               break; // ignore whitespace
-         for (; i < message.length; i++) {
-            if (message[i] == '\r')
-               break;
-            token ~= message[i];
-         }
-         i++;
-         if (message[i] != '\n')
-            return req;
-         req.headers[key] = token;
-      }
+		// get headers
+		string key = "";
+		for (; i < msg.length; i++) {
+			token = "";
+			key = "";
+			if (msg[i] == '\r')
+				break;
+			for (; i < msg.length; i++) {
+				if (msg[i] == ':' || msg[i].isWhite)
+					break;
+				token ~= msg[i];
+			}
+			i++;
+			key = token;
+			token = "";
+			for (; i < msg.length; i++)
+				if (!msg[i].isWhite)
+					break; // ignore whitespace
+			for (; i < msg.length; i++) {
+				if (msg[i] == '\r')
+					break;
+				token ~= msg[i];
+			}
+			i++;
+			if (msg[i] != '\n')
+				return req;
+			req.headers[key] = token;
+		}
 
-      i++;
-      if (message[i] != '\n')
-         return req;
-      i++;
+		i++;
+		if (msg[i] != '\n')
+			return req;
+		i++;
 
-      req.message = message[i .. $];
-      req.done = true;
-      Request ans = req;
-      req = Request.init;
-      data = [];
-      return ans;
-   }
+		req.message = msg[i .. $];
+		req.done = true;
+		Request ans = req;
+		req = Request();
+		data = [];
+		return ans;
+	}
 }
--- src/websocketd/server.d
+++ src/websocketd/server.d
@@ -9,222 +9,205 @@ import websocketd.frame;
 alias PeerID = size_t;
 
 class WebSocketState {
-   Socket socket;
-   bool handshaken;
-   Frame[] frames = [];
-   immutable PeerID id;
-   immutable Address address;
-   string path;
+	Socket socket;
+	bool handshaken;
+	Frame[] frames;
+	immutable PeerID id;
+	immutable Address address;
+	string path;
 
-   @disable this();
+	@disable this();
 
-   this(PeerID id, Socket socket) {
-      this.socket = socket;
-      this.id = id;
-      this.address = cast(immutable Address)socket.remoteAddress;
-   }
+	this(PeerID id, Socket socket) {
+		this.socket = socket;
+		this.id = id;
+		this.address = cast(immutable Address)socket.remoteAddress;
+	}
 
-   void performHandshake(ubyte[] message) {
-      import std.base64 : Base64;
-      import std.digest.sha : sha1Of;
-      import std.conv : to;
+	void performHandshake(ubyte[] message) {
+		import std.base64 : Base64;
+		import std.digest.sha : sha1Of;
+		import std.conv : to;
 
-      assert(!handshaken);
-      enum MAGIC = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11",
-           KEY = "Sec-WebSocket-Key";
-      Request request = Request.parse(message);
-      if (!request.done || KEY !in request.headers)
-         return;
-      this.path = request.path;
-      auto accept = Base64.encode(sha1Of(request.headers[KEY] ~ MAGIC));
-      assert(socket.isAlive);
-      socket.send(
-            "HTTP/1.1 101 Switching Protocol\r\n" ~ "Upgrade: websocket\r\n" ~ "Connection: Upgrade\r\n"
-            ~ "Sec-WebSocket-Accept: " ~ accept ~ "\r\n\r\n");
-      handshaken = true;
-   }
+		assert(!handshaken);
+		enum MAGIC = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11",
+			 KEY = "Sec-WebSocket-Key";
+		auto request = Request.parse(message);
+		if (!request.done || KEY !in request.headers)
+			return;
+		this.path = request.path;
+		auto accept = Base64.encode(sha1Of(request.headers[KEY] ~ MAGIC));
+		assert(socket.isAlive);
+		socket.send(
+			"HTTP/1.1 101 Switching Protocol\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: "
+			~ accept ~ "\r\n\r\n");
+		handshaken = true;
+	}
 }
 
 abstract class WebSocketServer {
+	import std.traits;
 
-   private WebSocketState[PeerID] sockets;
-   private Socket listener;
-   private size_t maxConnections;
+	protected WebSocketState[PeerID] sockets;
+	private Socket listener;
+	size_t maxConnections;
 
-   abstract void onOpen(PeerID s, string path);
-   abstract void onTextMessage(PeerID s, string s);
-   abstract void onBinaryMessage(PeerID s, ubyte[] o);
-   abstract void onClose(PeerID s);
+	abstract void onOpen(PeerID s, string path);
+	abstract void onTextMessage(PeerID s, string s);
+	abstract void onBinaryMessage(PeerID s, ubyte[] o);
+	abstract void onClose(PeerID s);
 
-   private static PeerID counter = 0;
+	protected static PeerID counter = 0;
 
-   this() { listener = new TcpSocket(); }
+	this() { listener = new TcpSocket(); }
 
-   private void add(Socket socket) {
-      if (sockets.length >= maxConnections) {
-         infof("Maximum number of connections reached (%d)", maxConnections);
-         socket.close();
-         return;
-      }
-      auto s = new WebSocketState(counter++, socket);
-      infof("Acception connection from %s (id=%s)", socket.remoteAddress, s.id);
-      sockets[s.id] = s;
-   }
+	private void add(Socket socket) {
+		if (sockets.length >= maxConnections) {
+			infof("Maximum number of connections reached (%u)", maxConnections);
+			socket.close();
+			return;
+		}
+		infof("Acception connection from %s (id=%u)", socket.remoteAddress, counter);
+		sockets[counter] = new WebSocketState(counter, socket);
+		counter++;
+	}
 
-   private void remove(WebSocketState socket) {
-      sockets.remove(socket.id);
-      infof("Closing connection with client id %s", socket.id);
-      if (socket.socket.isAlive)
-         socket.socket.close();
-      onClose(socket.id);
-   }
+	void remove(WebSocketState socket) {
+		sockets.remove(socket.id);
+		infof("Closing connection with client id %u", socket.id);
+		if (socket.socket.isAlive)
+			socket.socket.close();
+		onClose(socket.id);
+	}
 
-   private void handle(WebSocketState socket, ubyte[] message) {
-      import std.conv : to;
-      import std.algorithm : swap;
+	private void handle(WebSocketState socket, ubyte[] message) {
+		import std.conv : to;
+		import std.algorithm : swap;
 
-      string processId = typeof(this).stringof ~ socket.id.to!string;
-      if (socket.handshaken) {
-         Frame prevFrame = processId.parse(message);
-         Frame newFrame, temp;
-         do {
-            handleFrame(socket, prevFrame);
-            newFrame = processId.parse([]);
-            swap(newFrame, prevFrame);
-         }
-         while (newFrame != prevFrame);
-      } else {
-         socket.performHandshake(message);
-         if (socket.handshaken)
-            infof("Handshake with %s done (path=%s)", socket.id, socket.path);
-         onOpen(socket.id, socket.path);
-      }
-   }
+		string processId = typeof(this).stringof ~ socket.id.to!string;
+		if (socket.handshaken) {
+			Frame prevFrame = processId.parse(message);
+			Frame newFrame, temp;
+			do {
+				handleFrame(socket, prevFrame);
+				newFrame = processId.parse([]);
+				swap(newFrame, prevFrame);
+			} while (newFrame != prevFrame);
+		} else {
+			socket.performHandshake(message);
+			if (socket.handshaken)
+				infof("Handshake with %d done (path=%s)", socket.id, socket.path);
+			onOpen(socket.id, socket.path);
+		}
+	}
 
-   private void handleFrame(WebSocketState socket, Frame frame) {
-      tracef("From client %s received frame: done=%s; fin=%s; op=%s; length=%d", socket.id, frame.done, frame.fin, frame.op, frame.length);
-      if (!frame.done)
-         return;
-      final switch (frame.op) {
-         case Op.CONT:
-            return handleCont(socket, frame);
-         case Op.TEXT:
-            return handleText(socket, frame);
-         case Op.BINARY:
-            return handleBinary(socket, frame);
-         case Op.CLOSE:
-            return handleClose(socket, frame);
-         case Op.PING:
-            return handlePing(socket, frame);
-         case Op.PONG:
-            return handlePong(socket, frame);
-      }
-   }
+	private void handleFrame(WebSocketState socket, Frame frame) {
+		tracef("From client %s received frame: done=%s; fin=%s; op=%s; length=%u",
+			socket.id, frame.done, frame.fin, frame.op, frame.length);
+		if (!frame.done)
+			return;
+		final switch (frame.op) {
+			case Op.CONT: return handleCont(socket, frame);
+			case Op.TEXT: return handleText(socket, frame);
+			case Op.BINARY: return handleBinary(socket, frame);
+			case Op.CLOSE: return remove(socket);
+			case Op.PING:
+				socket.socket.send(Frame(true, Op.PONG, false, 0, [0, 0, 0, 0], true, []).serialize);
+				return;
+			case Op.PONG: return tracef("Received pong from %s", socket.id);
+		}
+	}
 
-   private void handleCont(WebSocketState socket, Frame frame) {
-      assert(socket.frames.length > 0);
-      if (frame.fin) {
-         Op originalOp = socket.frames[0].op;
-         ubyte[] data = [];
-         for (size_t i = 0; i < socket.frames.length; i++)
-            data ~= socket.frames[i].data;
-         data ~= frame.data;
-         socket.frames = [];
-         if (originalOp == Op.TEXT)
-            onTextMessage(socket.id, cast(string)data);
-         else if (originalOp == Op.BINARY)
-            onBinaryMessage(socket.id, data);
-      } else
-         socket.frames ~= frame;
-   }
+	private void handleCont(WebSocketState socket, Frame frame)
+	in (socket.frames.length > 0)
+	{
+		if (!frame.fin) {
+			socket.frames ~= frame;
+			return;
+		}
+		Op originalOp = socket.frames[0].op;
+		ubyte[] data = [];
+		for (size_t i = 0; i < socket.frames.length; i++)
+			data ~= socket.frames[i].data;
+		data ~= frame.data;
+		socket.frames = [];
+		if (originalOp == Op.TEXT)
+			onTextMessage(socket.id, cast(string)data);
+		else if (originalOp == Op.BINARY)
+			onBinaryMessage(socket.id, data);
+	}
 
-   private void handleText(WebSocketState socket, Frame frame) {
-      assert(socket.frames.length == 0);
-      if (frame.fin)
-         onTextMessage(socket.id, cast(string)frame.data);
-      else
-         socket.frames ~= frame;
-   }
+	private void handleText(WebSocketState socket, Frame frame)
+	in (socket.frames.length == 0)
+	{
+		if (frame.fin)
+			onTextMessage(socket.id, cast(string)frame.data);
+		else
+			socket.frames ~= frame;
+	}
 
-   private void handleBinary(WebSocketState socket, Frame frame) {
-      assert(socket.frames.length == 0);
-      if (frame.fin)
-         onBinaryMessage(socket.id, frame.data);
-      else
-         socket.frames ~= frame;
-   }
+	private void handleBinary(WebSocketState socket, Frame frame)
+	in (socket.frames.length == 0)
+	{
+		if (frame.fin)
+			onBinaryMessage(socket.id, frame.data);
+		else
+			socket.frames ~= frame;
+	}
 
-   private void handleClose(WebSocketState socket, Frame frame) {
-      remove(socket);
-   }
+	public void send(T)(PeerID dest, T msg){
+		auto dst = dest in sockets;
+		if (!dst) {
+			warningf("Tried to send a message to %s which is not connected", dest);
+			return;
+		}
+		static if (isSomeString!T) {
+			import std.string : representation;
 
-   private void handlePing(WebSocketState socket, Frame frame) {
-      socket.socket.send(Frame(true, Op.PONG, false, 0, [0, 0, 0, 0], true, []).serialize);
-   }
+			auto bytes = msg.representation;
+			auto frame = Frame(true, Op.TEXT, false, msg.length, [0, 0, 0, 0], true, bytes.dup);
+		} else {
+			alias bytes = msg;
+			auto frame = Frame(true, Op.BINARY, false, msg.length, [0, 0, 0, 0], true, msg);
+		}
+		auto data = frame.serialize;
+		tracef("Sending %u bytes to %s in one frame of %u bytes long", bytes.length, dest, data.length);
+		(*dst).socket.send(data);
+	}
 
-   private void handlePong(WebSocketState socket, Frame frame) {
-      tracef("Received pong from %s", socket.id);
-   }
+	public void run(ushort port, size_t maxConnections = 1000, size_t bufferSize = 1024)() {
+		this.maxConnections = maxConnections;
 
-   public void sendText(PeerID dest, string message) {
-      if (dest !in sockets) {
-         warningf("Tried to send a message to %s which is not connected", dest);
-         return;
-      }
-      import std.string : representation;
+		listener.blocking = false;
+		listener.bind(new InternetAddress("127.0.0.1", port));
+		listener.listen(10);
 
-      auto bytes = message.representation.dup;
-      auto frame = Frame(true, Op.TEXT, false, message.length, [0, 0, 0, 0], true, bytes);
-      auto serial = frame.serialize;
-      tracef("Sending %d bytes to %s in one frame of %d bytes long", bytes.length, dest, serial.length);
-      sockets[dest].socket.send(serial);
-   }
+		infof("Listening on port: %u", port);
+		infof("Maximum allowed connections: %u", maxConnections);
 
-   public void sendBinary(PeerID dest, ubyte[] message) {
-      if (dest !in sockets) {
-         warningf("Tried to send a message to %s which is not connected", dest);
-         return;
-      }
-      auto frame = Frame(true, Op.BINARY, false, message.length, [0, 0, 0, 0], true, message);
-      auto serial = frame.serialize;
-      tracef("Sending %d bytes to %s in one frame of %d bytes long", message.length, dest, serial.length);
-      sockets[dest].socket.send(serial);
-   }
+		auto set = new SocketSet(maxConnections + 1);
+		for (;;) {
+			set.add(listener);
+			foreach (id, s; sockets)
+				set.add(s.socket);
+			Socket.select(set, null, null);
 
-   public void run(ushort port, size_t maxConnections = 1000, size_t bufferSize = 1024)() {
-      this.maxConnections = maxConnections;
+			foreach (id, socket; sockets) {
+				if (!set.isSet(socket.socket))
+					continue;
+				ubyte[bufferSize] buffer;
+				long receivedLength = socket.socket.receive(buffer[]);
+				tracef("Received %u bytes from %s", receivedLength, socket.id);
+				if (receivedLength > 0)
+					handle(socket, buffer[0 .. receivedLength]);
+				else
+					remove(socket);
+			}
 
-      listener.blocking = false;
-      listener.bind(new InternetAddress(port));
-      listener.listen(10);
+			if (set.isSet(listener))
+				add(listener.accept());
 
-      infof("Listening on port: %d", port);
-      infof("Maximum allowed connections: %d", maxConnections);
-
-      auto set = new SocketSet(maxConnections + 1);
-      for (;;) {
-         set.add(listener);
-         foreach (id, s; sockets)
-            set.add(s.socket);
-         Socket.select(set, null, null);
-
-         foreach (id, socket; sockets) {
-            if (!set.isSet(socket.socket))
-               continue;
-            ubyte[bufferSize] buffer;
-            long receivedLength = socket.socket.receive(buffer[]);
-            tracef("Received %d bytes from %s", receivedLength, socket.id);
-            if (receivedLength > 0) {
-               handle(socket, buffer[0 .. receivedLength]);
-               continue;
-            }
-            remove(socket);
-         }
-
-         if (set.isSet(listener))
-            add(listener.accept());
-
-         set.reset();
-      }
-   }
+			set.reset();
+		}
+	}
 }
